a1fa98ccdba01ef7aa113d835259eb907a823e0a
Tarlan Huseynov <57084438+WarDove@users.noreply.github.com>
2022-07-20 16:34:28 +0400

    Update bastion.sh

--- jenkins/bastion.sh
+++ jenkins/bastion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in `seq 1 2 5`
   do  
-    ssh-keyscan -t rsa,dsa 10.0.${i}.4 >> ~/.ssh/known_hosts
+    grep -q "10.0.${i}.4" ~/.ssh/known_hosts || ssh-keyscan -t rsa,dsa 10.0.${i}.4 >> ~/.ssh/known_hosts
     scp /tmp/artifact.tgz ubuntu@10.0.${i}.4:/tmp; ssh ubuntu@10.0.${i}.4 "sudo killall main"
     ssh ubuntu@10.0.${i}.4 "tar xvzf /tmp/artifact.tgz --directory /home/ubuntu"
     ssh ubuntu@10.0.${i}.4 "sudo ./main &" &
